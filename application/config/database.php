f03c4fedd9e30470d785ed064057680200d3c5c8
root <root@www.telesport.cn>
2014-11-16 08:45:12 -0500

    11/16/2014 21:45

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
-$db['default']['username'] = 'shibo';
-$db['default']['password'] = '';
-$db['default']['database'] = 'c9';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'telesportcuhk';
+$db['default']['database'] = 'telesport';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
